69b8212760c906c7b02d02e112cee8a7ee449d27
Pierre Jiji <jijipierre85@gmail.com>
2019-08-16 14:31:10 -0400

    MVC

---
+++ app/Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace app
+{
+  [TestClass]
+  public class Tests
+  {
+    [TestMethod]
+    public void TestMethod()
+    {
+      Assert.AreEqual("1", "1");
+    }
+  }
+}
